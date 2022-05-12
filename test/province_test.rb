32173a4c994f90ff23bc3d1ac3e6ac5814ff017c
Jonah Broyer <jkbsk8er@yahoo.com>
2022-05-12 16:23:32 -0700

    Passed negative demand test.

--- test/province_test.rb
+++ test/province_test.rb
@@ -41,4 +41,11 @@ class ProvinceTest < Minitest::Test
     assert_equal(-25, asia.shortfall)
     assert_equal(0, asia.profit)
   end
+
+  def test_negative_demand
+    asia = Province.new(sample_province_data)
+    asia.demand = -1
+    assert_equal(-26, asia.shortfall)
+    assert_equal(-10, asia.profit)
+  end
 end

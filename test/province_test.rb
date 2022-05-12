1a200f02dfef640fdde00f523fdfa1ae63ddb407
Jonah Broyer <jkbsk8er@yahoo.com>
2022-05-12 16:15:20 -0700

    Passed production setter test.

--- test/province_test.rb
+++ test/province_test.rb
@@ -15,4 +15,11 @@ class ProvinceTest < Minitest::Test
     asia = Province.new(sample_province_data)
     assert_equal(230, asia.profit)
   end
+
+  def test_production_setter
+    asia = Province.new(sample_province_data)
+    asia.producers[0].production = 20
+    assert_equal(-6, asia.shortfall)
+    assert_equal(292, asia.profit)
+  end
 end
